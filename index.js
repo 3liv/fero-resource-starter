d46e90dd34860d33e2fd63ff58a90917d355eb0e
aliversi <aliversi@gmail.com>
2019-01-28 00:50:56 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fero = require('fero')
+    , log = require('utilise/log')('[fero-resource-starter]')
+    , subscribe = require('fero-resource-subscriber')
+
+module.exports = (dir = __dirname) => 
+  async ({ name, path }) => 
+    await fero(name, async req =>
+      req.value.type == 'SUBSCRIBE' ? subscribe(req, path, dir)
+                        : [405, 'method not allowed'])
