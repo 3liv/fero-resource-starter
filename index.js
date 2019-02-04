62b7b51c62e8a832379b56d85d3a7b40daf43205
aliversi <aliversi@gmail.com>
2019-02-04 08:02:50 +0000

    update to prevent cold start

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fero = require('fero')
+    , { memoize } = require('async-decorators')
     , log = require('utilise/log')('[fero-resource-starter]')
-    , subscribe = require('@3liv/fero-resource-subscriber')
+    , subscribe = memoize(require('@3liv/fero-resource-subscriber'))
 
-module.exports = (dir = __dirname) => 
-  async ({ name, path }) => 
+module.exports = (dir = __dirname, wp = false) => 
+  async ({ name, path, body = '', headers = '' }) => {
+    
+    //warm up to prevent cold start
+    subscribe(dir, { name, path, body, headers }, { wp })
+
     await fero(name, async req =>
-      req.value.type == 'SUBSCRIBE' ? subscribe(req, path, dir)
+      req.value.type == 'SUBSCRIBE' ? subscribe(dir, { name, path, body, headers}, { wp })
                         : [405, 'method not allowed'])
+  }
