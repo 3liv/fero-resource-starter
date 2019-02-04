887e23e9c1e7855daea3315c4019b25eae9d6e9f
aliversi <aliversi@gmail.com>
2019-02-04 13:23:36 +0000

    add warm up and caching using p-cache

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const fero = require('fero')
-    , { memoize } = require('async-decorators')
+    , pCache = require('p-cache')({
+        label: 'fero-service-cache',
+        // pass args to lru-cache here
+        max: 1000
+      })
     , log = require('utilise/log')('[fero-resource-starter]')
-    , subscribe = memoize(require('@3liv/fero-resource-subscriber'))
+    , subscribe = pCache(require('@3liv/fero-resource-subscriber'))
 
 module.exports = (dir = __dirname, wp = false) => 
   async ({ name, path, body = '', headers = '' }) => {
     
     //warm up to prevent cold start
-    subscribe(dir, { name, path, body, headers }, { wp })
+    log(`Warming up ${name}`)
+    await subscribe(dir, { name, path, body, headers }, { wp })
 
     await fero(name, async req =>
       req.value.type == 'SUBSCRIBE' ? subscribe(dir, { name, path, body, headers}, { wp })
